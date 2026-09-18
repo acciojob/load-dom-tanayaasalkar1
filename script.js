0b5e1969cb96c9edaf57520ce7c5614fe79b1c1d
Tanaya Asalkar <145457627+tanayaasalkar1@users.noreply.github.com>
2026-09-18 21:23:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    let p = document.createElement('p');
+    p.innerText = "DOM load success";
+
+    document.body.appendChild(p);
+});
